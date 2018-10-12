2510a75b7a0f903cb6ac85788ebb7693a8806007
sugihararen <sugihara@dptr.jp>
2018-10-12 18:55:35 +0900

    アクションで変数を定義しよう

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
+  
+  #投稿一覧ページ
   def index
+
+    @posts =
+      [
+        "今日からProgateでRailsの勉強するよー！",
+        "投稿一覧ページ作成中"
+      ]
+
+    end
   end
-end
