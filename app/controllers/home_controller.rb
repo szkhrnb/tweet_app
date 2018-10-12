d175c976720cbcad28bc19f7cfc333b0501fd308
sugihararen <sugihara@dptr.jp>
2018-10-12 16:15:48 +0900

    サービス紹介ページを追加しよう

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  
+  #トップページ
   def top
   end
+  
+  #サービス紹介ページ
+  def about
+  
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  #トップページ
   get "top" => "home#top"
+  #サービス紹介ページ
+  get "about" => "home#about"
+
 end
