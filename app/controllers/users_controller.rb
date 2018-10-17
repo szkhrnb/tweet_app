b29c2c0e97177bd01680b0b6e01967db52d36a14
sugihararen <sugihara@dptr.jp>
2018-10-17 11:49:31 +0900

    画像を保存しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,10 +33,17 @@ class UsersController < ApplicationController
   end
   
 
-  def update  
+  def update
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    
+    
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}",image.read)
+    end
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
       redirect_to("/users/#{@user.id}")
@@ -44,6 +51,5 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
-
-
+  
 end
